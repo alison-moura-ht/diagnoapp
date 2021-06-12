45de511ba0fb7e6bdc641c79c1b53084d41c44ae
Alison Moura <alison.moura@hightechcursos.com.br>
2021-06-12 12:17:46 -0400

    Fazendo função do login

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 let db = new Dexie("diagnoapp-db")
 
 db.version(2).stores(
-  { usuarios: '++id,nome,&email,ativo' }
+  { usuarios: '++id,nome,&email,ativo,senha,[email+senha]' }
 )
 
-// db.usuarios.put({nome: "José da Silva", email: "jose.silva@hightechcursos.com.br", ativo: true})
+async function login() {
+  let email = document.getElementById("email").value
+  let senha = document.getElementById("senha").value
+
+  let usuarioEncontrado = await db.usuarios.get({ email: email, senha: senha })
+  console.log(usuarioEncontrado)
+  if(usuarioEncontrado) {
+    if(usuarioEncontrado.ativo) {
+      alert("Usuário encontrado")
+    } else {
+      alert("Usuário está inativo")
+    }
+  }
+}
+
+// db.usuarios.put({nome: "Zé da Silva", email: "ze@htcursos.com", ativo: false, senha: "123"})
