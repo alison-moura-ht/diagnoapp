b12a6bd3ea2efdcb864a44f48477fa9a1fb71629
Alison Moura <alison.moura@hightechcursos.com.br>
2021-07-24 10:16:42 -0400

    Criando seleção das opções

--- controllers/questionario-controller.js
+++ controllers/questionario-controller.js
@@ -4,11 +4,15 @@ let questionario = [
         opcoes: [
             {
                 descricao: "Sim",
-                diagnostico: "Ótimo, esse é o caminho"
+                diagnostico: "Ótimo, esse é o caminho",
             },
             {
                 descricao: "Não",
-                diagnostico: "Que pena, você precisa melhorar"
+                diagnostico: "Que pena, você precisa melhorar",
+            },
+            {
+                descricao: "Mais ou menos",
+                diagnostico: "Mais pra mais? Ou mais pra menos?"
             }
         ]
     },
@@ -63,7 +67,45 @@ function carregarQuestao() {
     } else {
         document.getElementById("btnVoltar").classList.remove("d-none")
     }
+
+    carregarOpcoes()
 }
+
+function carregarOpcoes() {
+    let opcoes = questionario[posicaoQuestaoAtual].opcoes
+    let divOpcoes = document.getElementById("opcoes")
+
+    divOpcoes.innerHTML = "" // "Limpar" opções anteriores
+
+    for (let cont = 0; cont < opcoes.length; cont++) {
+        let opcaoAtual = opcoes[cont]
+        let btnOpcao = document.createElement("button") // cria a tag <button>
+        btnOpcao.onclick = () => selecionarOpcao(cont)
+        btnOpcao.innerHTML = opcaoAtual.descricao // Texto do botão
+        btnOpcao.classList.add("btn-opcao", "texto-branco", "w-full", "mt-20", "border-none", "border-2", "border-white", "border-solid", "py-10", "bg-transparent")
+        if (opcaoAtual.selecionado) {
+            btnOpcao.classList.remove("bg-transparent", "texto-branco")
+            btnOpcao.classList.add("bg-branco", "texto-verde")
+        }
+        divOpcoes.appendChild(btnOpcao)
+    }
+}
+
+function selecionarOpcao(posicaoEscolhida) {
+    limparSelecao()
+    let questaoAtual = questionario[posicaoQuestaoAtual]
+    let opcaoEscolhida = questaoAtual.opcoes[posicaoEscolhida]
+    opcaoEscolhida.selecionado = true
+    carregarOpcoes()
+}
+
+function limparSelecao() {
+    let opcoes = questionario[posicaoQuestaoAtual].opcoes
+    for (let cont = 0; cont < opcoes.length; cont++) {
+        opcoes[cont].selecionado = false
+    }
+}
+
 
 function proximo() {
     posicaoQuestaoAtual++
